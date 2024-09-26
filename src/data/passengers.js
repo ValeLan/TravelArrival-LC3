663ea569ad31d01bedb36b3d3216baebd143e464
ValeLan <vale.lanf@gmail.com>
2024-09-26 19:53:55 -0300

    Agregue una pequeña base de datos como ayuda visual

---
+++ src/data/passengers.js
@@ -0,0 +1,32 @@
+export const passengers = [
+    {
+        name: "Valentin",
+        surname: "Lanfranco",
+        dni: "41792624",
+        adress: "Catamarca 1955",
+        phoneNumber: "3401-515557",
+        district: "Centro",
+        school: "Manuel Belgrano",
+        schedule: "08:00"
+    },
+    {
+        name: "Candela",
+        surname: "Burgos",
+        dni: "44864201",
+        adress: "Jujuy 9234",
+        phoneNumber: "3401-516667",
+        district: "Sur",
+        school: "Moreno Belgrano",
+        schedule: "08:30"
+    },
+    {
+        name: "Keila",
+        surname: "Pagano",
+        dni: "43067234",
+        adress:"Formosa 1955",
+        phoneNumber: "3401-517777",
+        district: "Norte",
+        school: "Pepe Belgrano",
+        schedule: "08:00"
+    }
+]
