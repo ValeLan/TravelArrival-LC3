31cc2a4fb386efd62d7e6f3c619e9a7c9f510bc8
Keila <keilapagano@gmail.com>
2024-11-09 09:31:43 -0300

    agregue cambios con bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,16 @@ import { travels } from './data/travels'
 import { useState } from 'react';
 import NotFound from './components/routes/NotFound';
 import Admin from './components/admin/Admin';
-
-
-import { useFetch } from "./useFetch";
 import AdminCards from './components/admin/AdminCards';
+import { useFetch } from "./useFetch";
 
 function App() {
   const [isLogged, setIsLogged] = useState(false);
 
   const { data, error } = useFetch("/Travel/Historical", "GET");
+  const { data2, error2 } = useFetch("/Driver", "GET");
+  const { data3, error3 } = useFetch("/Passenger", "GET");
+  const { data4, error4 } = useFetch("/School", "GET");
 
   const loginHandler = () => {
     setIsLogged(true);
@@ -25,26 +26,23 @@ function App() {
   const router = createBrowserRouter([
     { path: "/", element: <Login /> },
     { path: "/login", element: <Login onLogin={loginHandler} /> },
-    {
-      path: "/driver",
+    
+    { path: "/driver",
       element: (
         <Protected isSignedIn={isLogged}>
-          <Driver />
-        </Protected>
-      ),
-    },
-    { path: "/driver", element: <Driver travels={travels} /> },
-    {
-      path: "/details/:id",
-      element: <DriverDetails travels={travels} />,
-    },
+          <Driver travels={travels}/> 
+        </Protected> )}, //esto esta bien?
+    
+    //{ path: "/driver", element: <Driver travels={travels} /> },
+    
+    { path: "/details/:id", element: <DriverDetails travels={travels} /> },
+
     { path: "*", element: <NotFound /> },
-    {
-      path: "/admin", element: <Admin />
-    },
-    {
-      path: "/admin-cards", element: <AdminCards />
-    },
+
+    { path: "/admin", element: <Admin /> },
+
+    {  path: "/admin-cards", element: <AdminCards  data={data} data2={data2} data3={data3} data4={data4}/> },
+    
   ]);
 
   return (
@@ -54,4 +52,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
--- src/components/admin/AdminCards.jsx
+++ src/components/admin/AdminCards.jsx
@@ -1,70 +1,104 @@
+
 import { useState } from 'react';
-import { passengers } from "../../data/passengers";
-import { drivers } from "../../data/drivers";
-import { districts } from "../../data/districts";
-import { schools } from "../../data/schools";
-import { travels } from "../../data/travels";
+// import { passengers } from "../../data/passengers";
+// import { drivers } from "../../data/drivers";
+// //import { districts } from "../../data/districts";
+// import { schools } from "../../data/schools";
+// import { travels } from "../../data/travels";
+import PropTypes from "prop-types";
 
+const AdminCards = ({data, data2, data3, data4}) => { 
+  const [selectedCard, setSelectedCard] = useState(null);
 
-const AdminCards = () => { 
-  const [selectedCard, setSelectedCard] = useState('drivers');
+  const renderCardContent = () => { 
+    if(selectedCard == null) {
+      return <p className="text-danger"> Error: {"Sin informaciòn"}</p>;
+    } else {
+    return data.map(i=> (
+      <div key={i.id} className="card mb-3">
+        <div className="card-body">
+        <h5 className="card-title"> {i.fullName} </h5>
+        <h3> {i.fullName} </h3>
+        </div>
+        </div>
+    ));
+  }};
 
-  const renderCardContent = () => {
-    switch (selectedCard) {
-      case 'passengers':
-        return passengers.map(passenger => (
-          <div key={passenger.id}>
-            <h3>{passenger.name}</h3>
-            <p>Dirección: {passenger.adress}</p>
-            <p>Distrito: {passenger.district}</p>
-          </div>
-        ));
-      case 'drivers':
-        return drivers.map(driver => (
-          <div key={driver.id}>
-            <h3>Nombre: {driver.name}</h3>
-            <p>Experiencia: {driver.experience}</p>
-          </div>
-        ));
-      case 'districts':
-        return districts.map(district => (
-          <div key={district.id}>
-            <p>Distrito: {district.name}</p>
-          </div>
-        ));
-      case 'schools':
-        return schools.map(school => (
-          <div key={school.id}>
-            <h3>Escuela: {school.name}</h3>
-          </div>
-        ));
-      case 'travels':
-        return travels.map(travel => (
-          <div key={travel.id}>
-            <p>Hora: {travel.hour}</p>
-            <p>Capacidad: {travel.capacity}</p>
-            <p>Estado: {travel.state}</p>
-          </div>
-        ));
-      default:
-        return <p style={{ color: 'red' }}>No se encontraron datos para {selectedCard}</p>;
-    }
+    return (
+      <div className="container mt-4">
+        <div className="btn-group mb-4" role="group">
+          <button type="button" style={{ backgroundColor: '#34759c' }} onClick={() => setSelectedCard(data)}>Drivers</button>
+          <button type="button" style={{ backgroundColor: '#2a7f62' }} onClick={() => setSelectedCard(data2)}>Schools</button>
+          <button type="button" style={{ backgroundColor: '#c4cf03' }} onClick={() => setSelectedCard(data3)}>Passengers</button>
+          <button type="button" style={{ backgroundColor: '#33c4ff' }} onClick={() => setSelectedCard(data4)}>Travels</button>
+        </div>
+        <div className="card-content">
+          {renderCardContent()}
+        </div>
+      </div>
+    );
   };
 
-  return (
-    <div>
-      <button type="button" style={{ backgroundColor: '#34759c' }} onClick={() => setSelectedCard('drivers')}>Drivers</button>
-      <button type="button" style={{ backgroundColor: '#2a7f62' }} onClick={() => setSelectedCard('schools')}>Schools</button>
-      <button type="button" style={{ backgroundColor: '#c4cf03' }} onClick={() => setSelectedCard('passengers')}>Passengers</button>
-      <button type="button" style={{ backgroundColor: '#ff5733' }} onClick={() => setSelectedCard('districts')}>Districts</button>
-      <button type="button" style={{ backgroundColor: '#33c4ff' }} onClick={() => setSelectedCard('travels')}>Travels</button>
-
-      
-      <div className="card-content">
-        {renderCardContent()}
-      </div>
-    </div>
-  );
-};
+  AdminCards.propTypes = {
+    data: PropTypes.array, data2: PropTypes.array, data3: PropTypes.array, data4: PropTypes.array,
+  };
 
 export default AdminCards;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+//     switch (selectedCard) {
+//       case 'passengers':
+//         return passengers.map(passenger => (
+//           <div key={passenger.id}>
+//             <h3>{passenger.name}</h3>
+//             <p>Dirección: {passenger.adress}</p>
+//             <p>Distrito: {passenger.district}</p>
+//           </div>
+//         ));
+//       case 'drivers':
+//         return drivers.map(driver => (
+//           <div key={driver.id}>
+//             <h3>Nombre: {driver.name}</h3>
+//             <p>Experiencia: {driver.experience}</p>
+//           </div>
+//         ));
+      
+//       case 'schools':
+//         return schools.map(school => (
+//           <div key={school.id}>
+//             <h3>Escuela: {school.name}</h3>
+//           </div>
+//         ));
+
+//       case 'travels':
+//         return travels.map(travel => (
+//           <div key={travel.id}>
+//             <p>Hora: {travel.hour}</p>
+//             <p>Capacidad: {travel.capacity}</p>
+//             <p>Estado: {travel.state}</p>
+//           </div>
+//         ));
+        
+//       default:
+//         return <p style={{ color: 'red' }}>No se encontraron datos para {selectedCard}</p>;
+//     }
+//   };
+
+  
+// };
+
