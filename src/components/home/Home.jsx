689bf50ddaa91158c4e5f44357c16306a6d14d37
Candela Burgos <90358734+Candela-Burgos@users.noreply.github.com>
2024-11-11 23:59:38 -0300

    Agregamos funcionalidad de admin

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,4 @@
-import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import { Button, Card, Col, Row } from "react-bootstrap";
 import "./home.css";
 import { useNavigate } from "react-router-dom";
 import { Slide, toast, ToastContainer } from "react-toastify";
